66205b0c8ff1cc5cd562b505e99fac8d2985caef
Furkan394 <furkanncakiroglu@gmail.com>
2022-09-01 20:10:25 +0300

    added validator for programming language update ops.

---
+++ src/Kodlama.io.Devs/Kodlama.io.Devs.Application/Features/ProgrammingLanguages/Commands/UpdateProgrammingLanguage/UpdateProgrammingLanguageCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Kodlama.io.Devs.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Commands.UpdateProgrammingLanguage
+{
+    public class UpdateProgrammingLanguageCommandValidator : AbstractValidator<ProgrammingLanguage>
+    {
+        public UpdateProgrammingLanguageCommandValidator()
+        {
+            RuleFor(p => p.Name).NotEmpty();
+        }
+    }
+}
