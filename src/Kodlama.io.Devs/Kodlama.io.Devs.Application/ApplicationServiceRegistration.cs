d23f852f61eba374bd9b60a80ef8592d1a0ecbe7
Furkan394 <furkanncakiroglu@gmail.com>
2022-09-01 16:36:34 +0300

    service registrations added

---
+++ src/Kodlama.io.Devs/Kodlama.io.Devs.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Rules;
+
+namespace Kodlama.io.Devs.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            services.AddScoped<ProgrammingLanguageBusinessRules>();
+
+            return services;
+        }
+    }
+}
--- src/Kodlama.io.Devs/WebAPI/Program.cs
+++ src/Kodlama.io.Devs/WebAPI/Program.cs
@@ -1,20 +1,27 @@
+using Core.CrossCuttingConcerns.Exceptions;
+using Kodlama.io.Devs.Application;
+using Kodlama.io.Devs.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddApplicationServices();
+builder.Services.AddPersistenceServices(builder.Configuration);
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+//if (app.Environment.IsProduction())
+    app.ConfigureCustomExceptionMiddleware();
 
 app.UseAuthorization();
 
