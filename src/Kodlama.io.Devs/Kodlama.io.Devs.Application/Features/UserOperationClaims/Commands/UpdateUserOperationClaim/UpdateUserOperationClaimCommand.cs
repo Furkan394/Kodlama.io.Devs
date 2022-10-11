bcb13e2fc80e056fadd27ba944140be61cdf0516
Furkan394 <furkanncakiroglu@gmail.com>
2022-10-11 12:27:08 +0300

    added updateUserOperationClaimCommand

---
+++ src/Kodlama.io.Devs/Kodlama.io.Devs.Application/Features/UserOperationClaims/Commands/UpdateUserOperationClaim/UpdateUserOperationClaimCommand.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using Kodlama.io.Devs.Application.Features.UserOperationClaims.Dtos;
+using Kodlama.io.Devs.Application.Features.UserOperationClaims.Rules;
+using Kodlama.io.Devs.Application.Services.Repositories;
+using MediatR;
+using static Kodlama.io.Devs.Domain.Constants.OperationClaims;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Security.Entities;
+
+namespace Kodlama.io.Devs.Application.Features.UserOperationClaims.Commands.UpdateUserOperationClaim
+{
+    public class UpdateUserOperationClaimCommand : IRequest<UpdatedUserOperationClaimDto>
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int OperationClaimId { get; set; }
+        public string[] Roles => new[] { Admin };
+
+        public class UpdateUserOperationClaimCommandHandler : IRequestHandler<UpdateUserOperationClaimCommand, UpdatedUserOperationClaimDto>
+        {
+            private readonly IUserOperationClaimRepository _userOperationClaimRepository;
+            private readonly IMapper _mapper;
+            private readonly UserOperationClaimBusinessRules _userOperationClaimBusinessRules;
+
+            public UpdateUserOperationClaimCommandHandler(IUserOperationClaimRepository userOperationClaimRepository,
+                                                          IMapper mapper,
+                                                          UserOperationClaimBusinessRules userOperationClaimBusinessRules)
+            {
+                _userOperationClaimRepository = userOperationClaimRepository;
+                _mapper = mapper;
+                _userOperationClaimBusinessRules = userOperationClaimBusinessRules;
+            }
+            public async Task<UpdatedUserOperationClaimDto> Handle(UpdateUserOperationClaimCommand request, CancellationToken cancellationToken)
+            {
+                await _userOperationClaimBusinessRules.UserOperationClaimCanNotBeDuplicatedWhenInserted(request.OperationClaimId);
+
+                UserOperationClaim? userOperationClaim = await _userOperationClaimRepository.GetAsync(u => u.Id == request.Id);
+
+                _userOperationClaimBusinessRules.UserOperationClaimShouldExistWhenRequested(userOperationClaim!);
+
+                _mapper.Map(request, userOperationClaim);
+                UserOperationClaim updatedUserOperationClaim = await _userOperationClaimRepository.UpdateAsync(userOperationClaim!);
+                UpdatedUserOperationClaimDto updatedUserOperationClaimDto = _mapper.Map<UpdatedUserOperationClaimDto>(updatedUserOperationClaim);
+
+                return updatedUserOperationClaimDto;
+            }
+        }
+    }
+}
--- src/Kodlama.io.Devs/WebAPI/Controllers/UserOperationClaimsController.cs
+++ src/Kodlama.io.Devs/WebAPI/Controllers/UserOperationClaimsController.cs
@@ -1,5 +1,6 @@
 ﻿using Kodlama.io.Devs.Application.Features.UserOperationClaims.Commands.CreateUserOperationClaim;
 using Kodlama.io.Devs.Application.Features.UserOperationClaims.Commands.DeleteUserOperationClaim;
+using Kodlama.io.Devs.Application.Features.UserOperationClaims.Commands.UpdateUserOperationClaim;
 using Kodlama.io.Devs.Application.Features.UserOperationClaims.Dtos;
 using Kodlama.io.Devs.Application.Features.UserOperationClaims.Queries.GetByUserIdUserOperationClaim;
 using Microsoft.AspNetCore.Http;
@@ -23,6 +24,13 @@ namespace WebAPI.Controllers
         {
             CreatedUserOperationClaimDto result = await Mediator.Send(createUserOperationClaimCommand);
             return Created("", result);
+        }
+
+        [HttpPost("update")]
+        public async Task<IActionResult> Update([FromBody] UpdateUserOperationClaimCommand updateUserOperationClaimCommand)
+        {
+            UpdatedUserOperationClaimDto result = await Mediator.Send(updateUserOperationClaimCommand);
+            return Ok(result);
         }
 
         [HttpPost("delete/{Id}")]
